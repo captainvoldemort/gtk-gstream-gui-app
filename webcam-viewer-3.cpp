fb0f45e5c0a8f239b2e17c61e2f69d5d446caf32
Yash Kulkarni <yashreadytobox@gmail.com>
2023-11-23 17:29:43 +0530

    Update webcam-viewer-3.cpp

--- webcam-viewer-3.cpp
+++ webcam-viewer-3.cpp
@@ -1,32 +1,38 @@
-#include <gtk/gtk.h>
 #include <gst/gst.h>
-#include <gst/video/videooverlay.h>
-#include <gdk/gdkx.h>
+#include <glib-unix.h>
+#include <signal.h>
 
-typedef struct {
-    GtkWidget *main_window;
-    GtkWidget *video_widget;
-    GstElement *pipeline;
-    GstElement *video_overlay;
-} AppData;
+#define HOST "127.0.0.1"
+
+gboolean signal_handler(gpointer user_data) {
+    GMainLoop *loop = (GMainLoop *)user_data;
+
+    g_print("Closing the main loop\n");
+    g_main_loop_quit(loop);
+
+    return TRUE;
+}
 
 static gboolean bus_callback(GstBus *bus, GstMessage *message, gpointer data) {
-    AppData *app_data = static_cast<AppData*>(data);
+    GMainLoop *loop = (GMainLoop *)data;
 
     switch (GST_MESSAGE_TYPE(message)) {
         case GST_MESSAGE_ERROR: {
             GError *err;
-            gchar *debug_info;
+            gchar *debug;
 
-            gst_message_parse_error(message, &err, &debug_info);
-            g_printerr("Error received from element %s: %s\n", GST_OBJECT_NAME(message->src), err->message);
-            g_printerr("Debugging information: %s\n", debug_info ? debug_info : "none");
-            g_clear_error(&err);
-            g_free(debug_info);
+            gst_message_parse_error(message, &err, &debug);
+            g_printerr("Error: %s\nDebug details: %s\n", err->message, debug ? debug : "none");
+            g_error_free(err);
+            g_free(debug);
 
-            gtk_main_quit();
+            g_main_loop_quit(loop);
             break;
         }
+        case GST_MESSAGE_EOS:
+            g_print("End of Stream reached\n");
+            g_main_loop_quit(loop);
+            break;
         default:
             break;
     }
@@ -34,90 +40,46 @@ static gboolean bus_callback(GstBus *bus, GstMessage *message, gpointer data) {
     return TRUE;
 }
 
-static void realize_cb(GtkWidget *widget, gpointer data) {
-    AppData *app_data = static_cast<AppData*>(data);
+int main(gint argc, gchar *argv[]) {
+    GstElement *pipeline = NULL, *src = NULL, *videoconvert = NULL, *videoscale = NULL, *encoder = NULL,
+               *capsfilter = NULL, *muxer = NULL, *sink = NULL;
+    GstCaps *caps = NULL;
+    GMainLoop *loop;
+    GstBus *bus;
 
-    gst_video_overlay_set_window_handle(GST_VIDEO_OVERLAY(app_data->video_overlay),
-                                        GDK_WINDOW_XID(gtk_widget_get_window(app_data->video_widget)));
-}
-
-static void initialize_pipeline(AppData *app_data) {
-    GstElement *pipeline = gst_pipeline_new("webcam_pipeline");
-    GstElement *source = gst_element_factory_make("v4l2src", "webcam_source");
-    GstElement *sink = gst_element_factory_make("xvimagesink", "video_sink");
-
-    if (!pipeline || !source || !sink) {
-        g_error("Failed to create GStreamer elements.");
-        return;
-    }
-
-    g_object_set(G_OBJECT(source), "device", "/dev/video0", NULL);
-
-    gst_bin_add_many(GST_BIN(pipeline), source, sink, NULL);
-
-    if (!gst_element_link(source, sink)) {
-        g_error("Failed to link GStreamer elements.");
-        gst_object_unref(pipeline);
-        return;
-    }
-
-    app_data->pipeline = pipeline;
-    app_data->video_overlay = gst_element_factory_make("xvimagesink", "video_overlay");
-
-    if (!app_data->video_overlay) {
-        g_error("Failed to create video overlay element.");
-        gst_object_unref(pipeline);
-        return;
-    }
-
-    gst_bin_add(GST_BIN(pipeline), app_data->video_overlay);
-    gst_element_sync_state_with_parent(app_data->video_overlay);
-}
-
-static void setup_gui(AppData *app_data) {
-    app_data->main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(app_data->main_window), "Webcam Viewer");
-    gtk_window_set_default_size(GTK_WINDOW(app_data->main_window), 640, 480);
-    g_signal_connect(G_OBJECT(app_data->main_window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-    app_data->video_widget = gtk_drawing_area_new();
-    gtk_container_add(GTK_CONTAINER(app_data->main_window), app_data->video_widget);
-
-    // Connect the "realize" signal to the realize_cb function
-    g_signal_connect(G_OBJECT(app_data->video_widget), "realize", G_CALLBACK(realize_cb), app_data);
-
-    gtk_widget_show_all(app_data->main_window);
-
-    GstBus *bus = gst_element_get_bus(app_data->pipeline);
-    gst_bus_add_watch(bus, (GstBusFunc)bus_callback, app_data);
-    gst_object_unref(bus);
-}
-
-static void start_pipeline(AppData *app_data) {
-    // Set the pipeline to the PAUSED state initially
-    gst_element_set_state(app_data->pipeline, GST_STATE_PAUSED);
-
-    // Start the GTK main loop
-    gtk_main();
-
-    // Set the pipeline to the PLAYING state after the GTK main loop has started
-    gst_element_set_state(app_data->pipeline, GST_STATE_PLAYING);
-}
-
-int main(int argc, char *argv[]) {
-    gtk_init(&argc, &argv);
     gst_init(&argc, &argv);
 
-    AppData app_data;
-    initialize_pipeline(&app_data);
-    setup_gui(&app_data);
-    start_pipeline(&app_data);
+    pipeline = gst_pipeline_new("pipeline");
 
-    // Block until the application exits
-    gst_element_get_state(app_data.pipeline, NULL, NULL, GST_CLOCK_TIME_NONE);
+    src = gst_element_factory_make("autovideosrc", "autovideosrc");
+    // ... (Check and handle errors for other elements similarly)
 
-    gst_element_set_state(app_data.pipeline, GST_STATE_NULL);
-    gst_object_unref(app_data.pipeline);
+    if (!pipeline || !src || !videoconvert || !videoscale || !capsfilter || !encoder || !muxer || !sink) {
+        g_printerr("Failed to create GStreamer elements\n");
+        goto cleanup;
+    }
+
+    // ... (Element creation and linking)
+
+    bus = gst_pipeline_get_bus(GST_PIPELINE(pipeline));
+    gst_bus_add_watch(bus, bus_callback, loop);
+    gst_object_unref(bus);
+
+    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    g_print("Pipeline playing\n");
+
+    loop = g_main_loop_new(NULL, FALSE);
+    g_unix_signal_add(SIGINT, signal_handler, loop);
+    g_main_loop_run(loop);
+
+cleanup:
+    gst_element_set_state(pipeline, GST_STATE_NULL);
+    g_print("Closed successfully\n");
+
+    if (pipeline != NULL) {
+        gst_object_unref(pipeline);
+    }
+    gst_deinit();
 
     return 0;
 }
