f79ffe121d715cea0f2180cd9d3406f8c572b793
Yash Kulkarni <yashreadytobox@gmail.com>
2023-11-23 17:39:03 +0530

    Update webcam-viewer.cpp

--- webcam-viewer.cpp
+++ webcam-viewer.cpp
@@ -1,185 +1,99 @@
 #include <gtk/gtk.h>
-
 #include <gst/gst.h>
-
 #include <gst/video/videooverlay.h>
-
-
-
-// Structure to hold application data
+#include <gdk/gdkx.h>  // Add this line for GDK_WINDOW_XID
 
 typedef struct {
-
-  GtkWidget *main_window; // Main application window
-
-  GtkWidget *video_widget; // Widget to hold the video overlay
-
-  GstElement *pipeline; // GStreamer pipeline
-
+    GtkWidget *main_window;
+    GtkWidget *video_widget;
+    GstElement *pipeline;
+    GstElement *video_sink;
 } AppData;
 
+static gboolean bus_callback(GstBus *bus, GstMessage *message, gpointer data) {
+    AppData *app_data = static_cast<AppData *>(data);
 
+    switch (GST_MESSAGE_TYPE(message)) {
+        case GST_MESSAGE_ERROR: {
+            GError *err;
+            gchar *debug_info;
 
-// Function to initialize GStreamer pipeline
+            gst_message_parse_error(message, &err, &debug_info);
+            g_printerr("Error received from element %s: %s\n", GST_OBJECT_NAME(message->src), err->message);
+            g_printerr("Debugging information: %s\n", debug_info ? debug_info : "none");
+            g_clear_error(&err);
+            g_free(debug_info);
+
+            gtk_main_quit();
+            break;
+        }
+        default:
+            break;
+    }
+
+    return TRUE;
+}
 
 static void initialize_pipeline(AppData *app_data) {
+    GstElement *pipeline = gst_pipeline_new("webcam_pipeline");
+    GstElement *source = gst_element_factory_make("v4l2src", "webcam_source");
+    GstElement *video_sink = gst_element_factory_make("autovideosink", "video_sink");
 
-  // Create GStreamer elements
+    if (!pipeline || !source || !video_sink) {
+        g_error("Failed to create GStreamer elements.");
+        return;
+    }
 
-  GstElement *pipeline = gst_pipeline_new("webcam_pipeline");
+    g_object_set(G_OBJECT(source), "device", "/dev/video0", NULL);
 
-  GstElement *source = gst_element_factory_make("v4l2src", "webcam_source");
+    gst_bin_add_many(GST_BIN(pipeline), source, video_sink, NULL);
 
-  GstElement *sink = gst_element_factory_make("autovideosink", "video_sink");
+    if (!gst_element_link(source, video_sink)) {
+        g_error("Failed to link GStreamer elements.");
+        gst_object_unref(pipeline);
+        return;
+    }
 
-
-
-  // Check if elements are created successfully
-
-  if (!pipeline || !source || !sink) {
-
-    g_error("Failed to create GStreamer elements.");
-
-    return;
-
-  }
-
-
-
-  // Set device for v4l2src (adjust the device path as needed)
-
-  g_object_set(G_OBJECT(source), "device", "/dev/video0", NULL);
-
-
-
-  // Add elements to the pipeline
-
-  gst_bin_add_many(GST_BIN(pipeline), source, sink, NULL);
-
-
-
-  // Link elements
-
-  if (!gst_element_link(source, sink)) {
-
-    g_error("Failed to link GStreamer elements.");
-
-    gst_object_unref(pipeline);
-
-    return;
-
-  }
-
-
-
-  // Set the pipeline to the application data structure
-
-  app_data->pipeline = pipeline;
-
+    app_data->pipeline = pipeline;
+    app_data->video_sink = video_sink;
 }
-
-
-
-// Function to set up the GTK window and video overlay
 
 static void setup_gui(AppData *app_data) {
+    app_data->main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(app_data->main_window), "Webcam Viewer");
+    gtk_window_set_default_size(GTK_WINDOW(app_data->main_window), 640, 480);
+    g_signal_connect(G_OBJECT(app_data->main_window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-  // Create the main window
+    app_data->video_widget = gtk_drawing_area_new();
+    gtk_container_add(GTK_CONTAINER(app_data->main_window), app_data->video_widget);
 
-  app_data->main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_widget_show_all(app_data->main_window);
 
-  gtk_window_set_title(GTK_WINDOW(app_data->main_window), "Webcam Viewer");
+    GstBus *bus = gst_element_get_bus(app_data->pipeline);
+    gst_bus_add_watch(bus, (GstBusFunc)bus_callback, app_data);
+    gst_object_unref(bus);
 
-  gtk_window_set_default_size(GTK_WINDOW(app_data->main_window), 640, 480);
-
-  g_signal_connect(G_OBJECT(app_data->main_window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-
-
-  // Create a drawing area for video overlay
-
-  app_data->video_widget = gtk_drawing_area_new();
-
-  gtk_container_add(GTK_CONTAINER(app_data->main_window), app_data->video_widget);
-
-
-
-  // Set up the video overlay
-
-  gst_video_overlay_set_window_handle(GST_VIDEO_OVERLAY(app_data->pipeline),
-
-                                    GPOINTER_TO_UINT(gtk_widget_get_window(app_data->video_widget)));
-
-
-
-  // Show all widgets
-
-  gtk_widget_show_all(app_data->main_window);
-
+    gst_video_overlay_set_window_handle(GST_VIDEO_OVERLAY(app_data->video_sink),
+                                        GDK_WINDOW_XID(gtk_widget_get_window(app_data->video_widget)));
 }
-
-
-
-// Function to start the GStreamer pipeline
 
 static void start_pipeline(AppData *app_data) {
-
-  // Set the pipeline to the playing state
-
-  gst_element_set_state(app_data->pipeline, GST_STATE_PLAYING);
-
+    gst_element_set_state(app_data->pipeline, GST_STATE_PLAYING);
 }
 
-
-
 int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+    gst_init(&argc, &argv);
 
-  // Initialize GTK and GStreamer
+    AppData app_data;
+    initialize_pipeline(&app_data);
+    setup_gui(&app_data);
+    start_pipeline(&app_data);
 
-  gtk_init(&argc, &argv);
+    gtk_main();
 
-  gst_init(&argc, &argv);
+    gst_element_set_state(app_data.pipeline, GST_STATE_NULL);
+    gst_object_unref(app_data.pipeline);
 
-
-
-  // Create application data structure
-
-  AppData app_data;
-
-
-
-  // Initialize GStreamer pipeline
-
-  initialize_pipeline(&app_data);
-
-
-
-  // Set up the GTK window and video overlay
-
-  setup_gui(&app_data);
-
-
-
-  // Start the GStreamer pipeline
-
-  start_pipeline(&app_data);
-
-
-
-  // Run the GTK main loop
-
-  gtk_main();
-
-
-
-  // Clean up resources
-
-  gst_element_set_state(app_data.pipeline, GST_STATE_NULL);
-
-  gst_object_unref(app_data.pipeline);
-
-
-
-  return 0;
-
+    return 0;
 }
